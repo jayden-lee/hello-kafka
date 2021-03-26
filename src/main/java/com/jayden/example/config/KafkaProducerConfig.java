28603aa2180b83228ce1409f0981f6c05e1949aa
jayden-lee <gwanggi.lee@gmail.com>
2021-03-27 00:31:39 +0900

    Update kafkaProducerConfig

--- src/main/java/com/jayden/example/config/KafkaProducerConfig.java
+++ src/main/java/com/jayden/example/config/KafkaProducerConfig.java
@@ -18,12 +18,19 @@ public class KafkaProducerConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapAddress;
 
+    private static final String GZIP = "gzip";
+    private static final int BATCH_SIZE = 16_384 * 4; // 64K
+    private static final int LINGER_MS = 100; // 100ms
+
     @Bean
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, GZIP);
+        configProps.put(ProducerConfig.BATCH_SIZE_CONFIG, BATCH_SIZE);
+        configProps.put(ProducerConfig.LINGER_MS_CONFIG, LINGER_MS);
 
         return new DefaultKafkaProducerFactory<>(configProps);
     }
